9c889971ecb70e93766307318d18e4d718c158bc
Merit <meritxelladech@gmail.com>
2024-11-17 23:48:44 +0100

    Decode base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Basic authentication module for API """
 
+import base64
 from api.v1.auth.auth import Auth
+from base64 import decode
 
 
 class BasicAuth(Auth):
@@ -22,7 +24,29 @@ class BasicAuth(Auth):
             return None
         if not isinstance(authorization_header, str):
             return None
-        if not authorization_header.startswith('Basic '):
+        if not authorization_header.startswith("Basic "):
             return None
         else:
-            return authorization_header.replace('Basic ', '')
+            return authorization_header.replace("Basic ", "")
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """Decode base64 string to its utf-8 string representation
+        Parameters:
+            - base64_authorization_header: str
+                Authorization header
+        Returns:
+            - the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # Decode the Base64 string to bytes
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            # Convert bytes to a UTF-8 string
+            return decoded_bytes.decode("utf-8")
+        except Exception:
+            # Return None if decoding fails
+            return None
