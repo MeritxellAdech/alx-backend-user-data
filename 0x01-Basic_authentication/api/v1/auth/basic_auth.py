853edce7b8c4118513704f5f9ed3774b6b4921b2
Merit <meritxelladech@gmail.com>
2024-11-18 10:03:29 +0100

    Extract user email and password from a decoded Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,19 +51,22 @@ class BasicAuth(Auth):
             # Return None if decoding fails
             return None
 
-    def extract_user_credentials(self,
-        decoded_base64_authorization_header: str) -> (str, str):
-        """ Extracts user email and password from a decoded Base64 string.
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """Extracts user email and password from a decoded Base64 string.
 
         Args:
-            decoded_base64_authorization_header (str): The decoded Base64 string.
+            decoded_base64_authorization_header:str
+                - The decoded Base64 string.
 
         Returns:
-            tuple: (user_email, user_password), or (None, None) if invalid."""
+            - tuple: (user_email, user_password)
+            - (None, None) if invalid."""
         if decoded_base64_authorization_header is None:
             return (None, None)
         if not isinstance(decoded_base64_authorization_header, str):
             return (None, None)
-        if ':' not in decoded_base64_authorization_header:
+        if ":" not in decoded_base64_authorization_header:
             return (None, None)
-        return decoded_base64_authorization_header.split(':')
+        return tuple(decoded_base64_authorization_header.split(":"))
